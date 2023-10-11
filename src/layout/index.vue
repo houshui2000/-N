0471bbf23e2c774a498539a92b890fa4e1836b18
侯水涛 <2283546335@qq.com>
2023-10-11 18:15:09 +0800

    微信提交 2023-10-11-1814

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -8,22 +8,19 @@
       </div>
     </article>
     <FooterVue />
-    <Login/>
-
+    <Login />
   </div>
 </template>
 <script setup>
-import {watch,watchEffect} from "vue";
+import { watchEffect } from 'vue'
 import NavVue from './components/top/index.vue'
 import FooterVue from './components/footer/index.vue'
 import Banner from './components/Banner/index.vue'
 import { useRoute } from 'vue-router'
 import Login from '@/components/Login/index.vue'
-import { useStore } from '@/pinia'
-import {weixinLogin} from "@/network/api.js"
+import { weixinLogin } from '@/network/api.js'
 const route = useRoute()
 
-const { useUsersStore } = useStore()
 // watch(()=>route.query.code,(value)=>{
 //   console.log("value",value)
 //   if(value){
@@ -32,14 +29,13 @@ const { useUsersStore } = useStore()
 //     console.log(useUsersStore.code)
 //   }
 // })
-watchEffect(()=>{
-  if(route.query.code){
-    console.log("dayin",route.query.code)
-    weixinLogin({code:route.query.code}).then(res=>{
-      console.log("接口參數",res)
+watchEffect(() => {
+  if (route.query.code) {
+    console.log('dayin', route.query.code)
+    weixinLogin({ code: route.query.code }).then((res) => {
+      console.log('接口參數', res)
     })
   }
-
 })
 </script>
 <style lang="scss" scoped>
