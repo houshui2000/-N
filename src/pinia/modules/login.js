ddc093603c6b3633a2f1b3fec9df1d4719847f4e
侯水涛 <2283546335@qq.com>
2023-10-11 18:20:17 +0800

    微信登陆hebing

--- src/pinia/modules/login.js
+++ src/pinia/modules/login.js
@@ -2,10 +2,10 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const loginDataInfo = defineStore('loginDataInfo', {
-    state: () => {
-        return {
-            code:'12344',
-            loginWX:true
-        }
-    },
+  state: () => {
+    return {
+      code: '12344',
+      loginWX: false
+    }
+  },
 })
