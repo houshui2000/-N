19b1a47c3fbfad2d58d9849f375b8ab3ec65c142
侯水涛 <2283546335@qq.com>
2023-10-12 09:32:02 +0800

    微信登陆实验 2023-1012-0932

--- src/components/Login/index.vue
+++ src/components/Login/index.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="login" v-if="loginStore.loginWX">
-    <div class="weixinBtn" @click="handleWinxinLogin">
-      微信登录
-    </div>
+    <div class="weixinBtn" @click="handleWinxinLogin">微信登录</div>
   </div>
 </template>
 
 <script setup>
-import {useRoute} from 'vue-router'
+// import { useRouter } from 'vue-router'
 import { useStore } from '@/pinia'
 const { loginStore } = useStore()
-const route = useRoute()
+// const router = useRouter()
 const handleWinxinLogin = () => {
-  window.open(`https://open.weixin.qq.com/connect/qrconnect?appid=wx9cb4b4cccc022d9e&redirect_uri=${encodeURI('https://www.95skins.com')}&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect`)
-  // route.query.code
-
+  window.open(
+    `https://open.weixin.qq.com/connect/qrconnect?appid=wx9cb4b4cccc022d9e&redirect_uri=${encodeURI(
+      'https://www.95skins.com'
+    )}&response_type=code&scope=snsapi_login&state=STATE#wechat_redirect`
+  )
 }
 </script>
 
@@ -25,14 +25,64 @@ const handleWinxinLogin = () => {
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(#000, .8);
+  background: rgba(#000, 0.8);
 
   .weixinBtn {
     width: 200px;
     height: 60px;
-    padding: 10px 20px;
+    padding: 20px;
     background: #453293;
     color: white;
   }
 }
 </style>
+<!--
+<template>
+  <div class="wxLogin">
+    <span class="title">微信扫一扫登录</span>
+    <div class="main_wx">
+      阿思达的
+      <wxlogin
+        v-if="appid && redirect_uri"
+        :appid="appid"
+        scope="snsapi_login"
+        :redirect_uri="redirect_uri"
+        :href="href"
+        :state="state"
+      ></wxlogin>
+    </div>
+    <p>请使用微信扫描二维码登录</p>
+  </div>
+</template>
+
+<script>
+import wxlogin from 'vue-wxlogin'
+// import api from '@api/index'
+export default {
+  name: 'wxLogin',
+  components: { wxlogin },
+  data() {
+    return {
+      appid: 'wx9cb4b4cccc022d9e&redirect',
+      redirect_uri: `${encodeURI('https://www.95skins.com')}`,
+      state: 'STATE#wechat_redirect',
+      href: '' // 自定义样式链接
+    }
+  },
+  mounted() {
+    this.getWeChatUrl()
+  },
+  methods: {
+    // 获取微信appid和回调地址redirect_uri，指定内嵌的路由地址weChatLogin
+    getWeChatUrl() {
+      // api.wachatQrUrl().then((res) => {
+      //   if (res && res.code === '0000') {
+      //     const data = res.data
+      //     this.appid = data.appId
+      //     this.redirect_uri = data.wxCallbackUrl + 'weChatLogin'
+      //   }
+      // })
+    }
+  }
+}
+</script> -->
